e8659053b3b2d257a56ede4bc01bc51224f67687
Lada <lada.shvetsova.1998@gmail.com>
2022-05-25 10:46:39 +0400

    Delete Main

--- src/animals/main/Main.java
+++
@@ -1,8 +0,0 @@
-package animals.main;
-
-public class Main {
-    public static void main(String[] args) {
-        Waterfowl fish = new Waterfowl(10,"Fish Vanya", 20);
-        fish.swim();
-    }
-}
